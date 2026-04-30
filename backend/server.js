a1aafc6cc39d30913edcacd1ab464a96e829e720
Franco Notarfrancesco <fnotarfrancesco@hotmail.com>
2026-04-30 19:39:22 -0300

    Fix cookie sameSite for cross-origin

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,7 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(express.static('./public'))
 app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(mongoSanitize())
 
 app.use(session({
@@ -48,7 +48,7 @@ app.use(session({
     maxAge: 24 * 60 * 60 * 1000,
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict'
+    sameSite: 'lax'
   }
 }))
 
