e24382ab890156dac07cf325e39bf245e27dd981
Franco Notarfrancesco <fnotarfrancesco@hotmail.com>
2026-04-30 23:11:29 -0300

    Set cookie secure=false

--- backend/server.js
+++ backend/server.js
@@ -46,8 +46,8 @@ app.use(session({
   store: null,
   cookie: {
     maxAge: 24 * 60 * 60 * 1000,
-    httpOnly: false,
-    secure: true,
+    httpOnly: true,
+    secure: false,
     sameSite: 'none'
   }
 }))
